cc1ff560150b53585c6c3999149b567310e6e08c
intissar120 <intissar.elqadi@gmail.com>
2024-10-10 10:59:06 +0200

    bdd

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
@@ -14,14 +14,13 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
-
     }
-    #[Route('/contact2', name: 'contact')]
+
+    #[Route('/contact2', name: 'app_contact2')]  // Renommer la route pour éviter le conflit
     public function about(): Response
     {
         return $this->render('contact/index.html.twig', [
-         'tableau' => ["Intissar","EL Qadi","Toulouse","intissar.elqadi@gmail.com"],
-
+            'tableau' => ["Intissar", "EL Qadi", "Toulouse", "intissar.elqadi@gmail.com"],
         ]);
     }
 }
